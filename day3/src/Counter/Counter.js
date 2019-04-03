25da187456a5bdaa773c05cadebee7301b4c7d64
magkuna <magkuna@gmail.com>
2019-04-03 11:02:32 +0200

    another way of creating buttons

--- day3/src/Counter/Counter.js
+++ day3/src/Counter/Counter.js
@@ -2,38 +2,38 @@ import React from 'react'
 import Button from '@material-ui/core/Button'
 
 class Counter extends React.Component {
-    constructor(){
+    constructor() {
         super()
-        this.state ={
+        this.state = {
             number: 10,
         }
     }
-    render() {
-        const inc = () => {
-        this.setState({number:this.state.number + 1}) 
+
+    inc() {
+        this.setState({ number: this.state.number + 1 })
     }
-        const dec = () => {
-            this.setState({number:this.state.number -1})
-        }
-        
+    dec() {
+        this.setState({ number: this.state.number - 1 })
+    }
+    render() {
         return (
-            <div class ="counter">
+            <div class="counter">
                 <h1>
                     {this.state.number}
                 </h1>
                 <Button
                     variant={"contained"}
                     color={"secondary"}
-                    onClick={inc}
-                   
+                    onClick={this.inc.bind(this)}
+
                 >
                     +
                 </Button>
                 <Button
                     variant={"contained"}
                     color={"primary"}
-                    onClick={dec}
-                   
+                    onClick={this.dec.bind(this)}
+
                 >
                     -
                 </Button>
