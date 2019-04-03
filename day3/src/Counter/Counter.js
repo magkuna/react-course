c51e48ec06613920dff2322851229a1f55e03300
magkuna <magkuna@gmail.com>
2019-04-03 11:40:40 +0200

    same in another way (constructor deleted)

--- day3/src/Counter/Counter.js
+++ day3/src/Counter/Counter.js
@@ -2,42 +2,41 @@ import React from 'react'
 import Button from '@material-ui/core/Button'
 
 class Counter extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            number: 10,
-        }
+
+    
+    state = {
+        number: 10,
     }
 
 
-    inc = () =>
-        this.setState({ number: this.state.number + 1 })
+        inc = () =>
+            this.setState({ number: this.state.number + 1 })
 
     dec = () =>
-        this.setState({ number: this.state.number - 1 })
+            this.setState({ number: this.state.number - 1 })
 
-render() {
-    return (
-        <div class="counter">
-            <h1>
-                {this.state.number}
-            </h1>
-            <Button
-                variant={"contained"}
-                color={"secondary"}
-                onClick={this.inc}
-            >
-                +
+    render() {
+            return (
+                <div class="counter">
+                    <h1>
+                        {this.state.number}
+                    </h1>
+                    <Button
+                        variant={"contained"}
+                        color={"secondary"}
+                        onClick={this.inc}
+                    >
+                        +
                 </Button>
-            <Button
-                variant={"contained"}
-                color={"primary"}
-                onClick={this.dec}
-            >
-                -
+                    <Button
+                        variant={"contained"}
+                        color={"primary"}
+                        onClick={this.dec}
+                    >
+                        -
                 </Button>
-        </div>
-    )
-}
-}
-export default Counter
+                </div>
+            )
+        }
+    }
+    export default Counter
