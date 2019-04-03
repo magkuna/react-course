0ce27ea69d9cc4feba12066f2e28e544fcc884d5
magkuna <magkuna@gmail.com>
2019-04-03 12:11:03 +0200

    couner done

--- day3/src/App.js
+++ day3/src/App.js
@@ -3,7 +3,11 @@ import React from 'react'
 import Counter from './Counter'
 
 const App = (props) => (
+    <div>
+    <Counter  number ={5}
+    />
     <Counter />
+    </div>
 )
 
 export default App
--- day3/src/Counter/Counter.js
+++ day3/src/Counter/Counter.js
@@ -2,41 +2,45 @@ import React from 'react'
 import Button from '@material-ui/core/Button'
 
 class Counter extends React.Component {
-
-    
     state = {
-        number: 10,
+        number: this.props.number || 0
     }
 
-
-        inc = () =>
-            this.setState({ number: this.state.number + 1 })
+    inc = () =>
+        this.setState({ number: this.state.number + 1 })
 
     dec = () =>
-            this.setState({ number: this.state.number - 1 })
+        this.setState({ number: this.state.number - 1 })
+
+
 
     render() {
-            return (
-                <div class="counter">
-                    <h1>
-                        {this.state.number}
-                    </h1>
-                    <Button
-                        variant={"contained"}
-                        color={"secondary"}
-                        onClick={this.inc}
-                    >
-                        +
+        return (
+            <div>
+                <h1>
+                    {this.state.number}
+                </h1>
+                <Button
+                    variant={"contained"}
+                    color={"secondary"}
+                    onClick={this.inc}
+                >
+                    +
                 </Button>
-                    <Button
-                        variant={"contained"}
-                        color={"primary"}
-                        onClick={this.dec}
-                    >
-                        -
+                <Button
+                    variant={"contained"}
+                    color={"primary"}
+                    onClick={this.dec}
+                >
+                    -
                 </Button>
-                </div>
-            )
-        }
+            </div>
+        )
     }
-    export default Counter
+}
+Counter.defaultProps = {
+    number: 0
+}
+
+
+export default Counter
