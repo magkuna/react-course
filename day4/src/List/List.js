c11ff4a94b890fdd077bb3a8fd385109c66a085b
magkuna <magkuna@gmail.com>
2019-04-04 13:38:10 +0200

    zamiana funkcji na component

--- day4/src/List/List.js
+++ day4/src/List/List.js
@@ -5,38 +5,32 @@ import NoLoaded from './NoLoaded'
 import NoUsers from './NoUsers'
 import Results from './Results'
 
-class List extends React.Component {
-    
-   
+const List = (props) => (
 
-    render() {
-        return (
             <div>
                 {
-                    this.props.isError ?
+                    props.isError ?
                         //'Error'
                         <Error />
                         :
-                        this.props.isLoading ?
+                        props.isLoading ?
                             //'Loading'
                             <Loading />
                             :
-                            !this.props.users ?
+                            !props.users ?
                                 // 'Data no loaded yet'
                                 <NoLoaded />
                                 :
-                                this.props.users.length === 0 ?
+                                props.users.length === 0 ?
                                 //'No users found'
                                 <NoUsers />
                                 :
                                 <Results 
-                                data={this.props.users}
+                                data={props.users}
                                 />
                 }
             </div>
         )
-    }
-}
 
 
 export default List
