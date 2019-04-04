2e56d15a06f8987545bcf4fab3cf5451b40570ef
magkuna <magkuna@gmail.com>
2019-04-04 14:58:06 +0200

    przefiltorwanie użytkowników według liter, spacji i znaków różnych

--- day4/src/Users/Users.js
+++ day4/src/Users/Users.js
@@ -28,7 +28,17 @@ class Users extends React.Component {
             this.state.users &&
             this.state.users.filter &&
             this.state.users.filter(
-                () => true
+                user => {
+                    const name = (user.name.first + user.name.last).toLowerCase()
+                    const searchTerm = this.state.searchTerm.toLowerCase()
+                    const searchTermWithoutSpaces = searchTerm.replace(/ /g, '')
+                    const searchTermWithoutDiacritics = searchTermWithoutSpaces.normalize('NFD').replace(/[\u0300-\u036f]/g, "")
+
+
+
+                    return name.includes(searchTermWithoutDiacritics)
+
+                }
             )
         )
 
